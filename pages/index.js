73191677068da178390c054d2ba148e0496ade66
Caleb Smith <caleb@myrvmail.com>
2022-04-14 15:53:34 -0500

    More work

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Grid from "../components/grid.js";
 import styles from "../styles/Home.module.scss";
 
@@ -24,10 +25,43 @@ export default function Home() {
 
         <Grid>
           <Grid.Item>
-            <Grid.Item.Title>Computers</Grid.Item.Title>
+            <Grid.Item.Title>Computers (In General)</Grid.Item.Title>
             <Grid.Item.Body>
-              I have always had an interest and a knack for computers, perhaps
-              because my mind itself operates unusually like unto one.
+              Read about how I have been obssessed with them since I was boy, my oldest
+              and most extensive hobby.
+            </Grid.Item.Body>
+          </Grid.Item>
+          <Grid.Item>
+            <Grid.Item.Title>Programming</Grid.Item.Title>
+            <Grid.Item.Body>
+              Of all computer activities, programming holds a special place as most
+              compulsive need, and I daresay the chief of my skills.
+              <br />
+              <Link href="/programming">
+                <a>Read of my obssession</a>
+              </Link>
+            </Grid.Item.Body>
+          </Grid.Item>
+          <Grid.Item>
+            <Grid.Item.Title>Writing</Grid.Item.Title>
+            <Grid.Item.Body>
+              Since second grade I have always been driven to write, fiction moreso in
+              my childhood and essays moreso in the present.
+            </Grid.Item.Body>
+          </Grid.Item>
+          <Grid.Item>
+            <Grid.Item.Title>Theology and Philosophy</Grid.Item.Title>
+            <Grid.Item.Body>
+              From the third grade lunch table until today, I have been incessantly
+              given to the study and disputation of theology, ethics, and, to a lesser
+              extent, philosophy, recently including political philosophy.
+            </Grid.Item.Body>
+          </Grid.Item>
+          <Grid.Item>
+            <Grid.Item.Title>Fantasy and the Medieval Donegality</Grid.Item.Title>
+            <Grid.Item.Body>
+              My imagination has always been captive to the world of swords, castles,
+              lore, and other worlds.
             </Grid.Item.Body>
           </Grid.Item>
         </Grid>
---
+++ pages/programming.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Programming() {
+  return (
+    <>
+      <Head>
+        <title>Programming ― Caleb's Petty Domain</title>
+      </Head>
+      <h1>Caleb the Programmer</h1>
+    </>
+  );
+}
