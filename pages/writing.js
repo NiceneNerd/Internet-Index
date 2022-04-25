99818286a3bf31fd95dc8de2994b756c01edb874
Caleb Smith <caleb@myrvmail.com>
2022-04-25 17:00:02 -0500

    Start on the writing page

--- pages/writing.js
+++ pages/writing.js
@@ -9,7 +9,24 @@ export default function Writing() {
       </Head>
       <Content>
         <h1>Caleb the Writer</h1>
-        <p>My love of writing began in the second grade and has </p>
+        <p>
+          My love of writing began in the second grade and has taken a number of odd
+          turns over the years. Most of my writing which is now still available is
+          specifically about theology and such, for which I have a separate page, but
+          here are a few other writings I have produced over the years. (Many are,
+          unfortunately, a bit old.)
+        </p>
+        <ul>
+          <li>
+            <a href="https://www.doctorwhotv.co.uk/the-doctor-dark-hero-no-more-56575.htm">
+              The Doctor: Dark Hero No More?
+            </a>{" "}
+            ― Doctor Who TV
+          </li>
+          <li>
+            <a href="https://talesofteretz.wordpress.com/">Tales of Teretz</a>
+          </li>
+        </ul>
       </Content>
     </>
   );
